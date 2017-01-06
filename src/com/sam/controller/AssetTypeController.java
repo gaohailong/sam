7fc7c48491cc69397c3b60090c1e976c3695d13e
BruceZhang54110 <zhw54110@163.com>
2017-01-06 11:35:46 +0800

    update from zxx1.6.am

--- src/com/sam/controller/AssetTypeController.java
+++ src/com/sam/controller/AssetTypeController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sam.entity.AssetDepartment;
+import com.sam.entity.AssetInfo;
 import com.sam.entity.AssetType;
+import com.sam.entity.Pager;
 import com.sam.service.AssetTypeService;
+import com.sam.service.DepartmentService;
+import com.sam.util.ConstantUtil;
 
 /**
  * 查询所有的设备类型
@@ -36,4 +41,69 @@ public class AssetTypeController {
 	public List<AssetType> findAllAssetType() throws Exception {
 		return assetTypeService.findAssetType();
 	}
-}
+		/**
+		 * @author zxx
+		 * 查询一级类型菜单使用get方式
+		 * @RequestMapping(value = "/findAllCategory", method = RequestMethod.GET)
+		 * @return
+		 */
+		@RequestMapping(value="/findAssetTypeFirst", method=RequestMethod.GET)
+		@ResponseBody
+		public List<AssetType> findAssetTypeFirst() {
+			try {
+				List<AssetType> assetTypeList = assetTypeService.findAssetTypeByAtlevel(0);
+				return assetTypeList;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		
+
+		/**
+		 * 查询二级部门
+		 * @param adlevel
+		 * @return
+		 * @author zxx
+		 */
+		@RequestMapping(value="/findAssetTypeSecond", method=RequestMethod.GET)
+		@ResponseBody
+		public List<AssetType> findAssetTypeSecond(Integer atlevel) {
+			System.out.println("进来了方法");
+			try {
+				System.out.println("atlevel:"+atlevel);
+				List<AssetType> assetTypeList = assetTypeService.findAssetTypeByAtlevel(atlevel);
+				return assetTypeList;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		/**
+		 * 根据资产类型查询资产
+		 * @author zxx
+		 * @param dname
+		 * @param page
+		 * @return
+		 */
+		@RequestMapping(value="/findAssetInfosByatname",method=RequestMethod.POST)
+		@ResponseBody
+		public Pager<AssetInfo> findAssetInfosByatname(String atname ,Integer page) {
+			try {
+				System.out.println("进入findAssetInfosByatname控制器");
+				System.out.println("atname:"+atname);
+				int pageNum = ConstantUtil.DEFAULT_PAGE_NUM;
+				if(page != null) {
+					pageNum = page;
+				}
+				int pageSize = ConstantUtil.DEFAULT_PAGE_SIZE;
+				Pager<AssetInfo> pager = assetTypeService.findAssetInfosByatname(atname, pageNum, pageSize);
+				System.out.println(pager.getDataList().size());
+				return pager;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+
